75eceb564af839fa59dfdc7f0156874a23731a63
ehime <dodomeki@gmail.com>
2019-07-02 13:37:29 -0700

    fixed the vault issues up till mounts, mounts is no longer an internal command and needs to be leveraged by the api

--- src/setup.sh
+++ src/setup.sh
@@ -1,34 +1,39 @@
-#!/bin/bash
+#!/bin/ash
 
 ## CONFIG LOCAL ENV
 echo "[*] Config local environment..."
 alias vault='docker-compose exec vault vault "$@"'
 export VAULT_ADDR=http://127.0.0.1:8200
 
+[ ! -d '_data' ]          && mkdir -p _data
+[ ! -f '_data/keys.txt' ] && touch    _data/keys.txt
+
 ## INIT VAULT
 echo "[*] Init vault..."
-vault init -address=${VAULT_ADDR} > ./_data/keys.txt
-export VAULT_TOKEN=$(grep 'Initial Root Token:' ./_data/keys.txt | awk '{print substr($NF, 1, length($NF)-1)}')
+vault operator init -address=${VAULT_ADDR} 2>&1 >| _data/keys.txt
+export VAULT_TOKEN=$(grep 'Initial Root Token:' _data/keys.txt |awk '{print substr($NF, 1, length($NF)-1)}')
 
 ## UNSEAL VAULT
 echo "[*] Unseal vault..."
-vault unseal -address=${VAULT_ADDR} $(grep 'Key 1:' ./_data/keys.txt | awk '{print $NF}')
-vault unseal -address=${VAULT_ADDR} $(grep 'Key 2:' ./_data/keys.txt | awk '{print $NF}')
-vault unseal -address=${VAULT_ADDR} $(grep 'Key 3:' ./_data/keys.txt | awk '{print $NF}')
+vault operator unseal -address=${VAULT_ADDR} $(grep 'Key 1:' _data/keys.txt |awk '{print $NF}')
+vault operator unseal -address=${VAULT_ADDR} $(grep 'Key 2:' _data/keys.txt |awk '{print $NF}')
+vault operator unseal -address=${VAULT_ADDR} $(grep 'Key 3:' _data/keys.txt |awk '{print $NF}')
 
 ## AUTH
 echo "[*] Auth..."
-vault auth -address=${VAULT_ADDR} ${VAULT_TOKEN}
+vault login -address=${VAULT_ADDR} ${VAULT_TOKEN}
 
 ## CREATE USER
 echo "[*] Create user... Remember to change the defaults!!"
-vault auth-enable  -address=${VAULT_ADDR} userpass
-vault policy-write -address=${VAULT_ADDR} admin ./config/admin.hcl
+vault auth enable  -address=${VAULT_ADDR} userpass
+vault policy write -address=${VAULT_ADDR} admin config/admin.hcl
 vault write -address=${VAULT_ADDR} auth/userpass/users/webui password=webui policies=admin
 
 ## CREATE BACKUP TOKEN
 echo "[*] Create backup token..."
-vault token-create -address=${VAULT_ADDR} -display-name="backup_token" | awk '/token/{i++}i==2' | awk '{print "backup_token: " $2}' >> ./_data/keys.txt
+vault token create -address=${VAULT_ADDR} -display-name="backup_token" |awk '/token/{i++}i==2' |awk '{print "backup_token: " $2}' 2>&1 >> _data/keys.txt
+
+## NEEDS TO BE FIXED, MOUNTS NO LONGER WORKS
 
 ## MOUNTS
 echo "[*] Creating new mount point..."
